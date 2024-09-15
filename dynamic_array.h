6475b1336aa19411e5a0a83c839534848171c446
girvel <widauka@ya.ru>
2024-09-15 22:31:03 +0500

    #

--- dynamic_array.h
+++ dynamic_array.h
@@ -20,15 +20,3 @@ void array_extend(DynamicArray *array, size_t extension_length, void *extension)
 void array_free(DynamicArray *array);
 
 #define ARRAY_AT(TYPE, ARRAY, INDEX) (*((TYPE *)array_at((ARRAY), (INDEX))))
-
-// #define ARRAY_EXTEND(ARRAY, EXTENSION_LENGTH, EXTENSION) { \
-//     DynamicArray *array = (ARRAY); \
-//     size_t extension_length = (EXTENSION_LENGTH); \
-//     __typeof__ (EXTENSION) extension = (EXTENSION); \
-//     \
-//     size_t base_index = array->_length; \
-//     array_increase_length(array, extension_length); \
-//     for (size_t i = 0; i < extension_length; i++) { \
-//         *((__typeof__ (EXTENSION)) array_at(*array, base_index + i)) = extension[i]; \
-//     } \
-// }
