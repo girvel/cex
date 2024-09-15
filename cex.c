06278ae8b91136579ac6ec8a2456dca319c3082a
girvel <widauka@ya.ru>
2024-09-16 00:41:05 +0500

    Entity & Components sketch

--- cex.c
+++ cex.c
@@ -3,9 +3,51 @@
 #include "framework/dynamic_array.h"
 #include "framework/dynamic_string.h"
 
-// typedef struct {
-//   void* components[3];
-// } Entity;
+
+typedef enum {
+    COMPONENT_NAME,
+    COMPONENT_POSITION,
+    COMPONENT_N,
+} COMPONENT;
+
+typedef struct {
+  DynamicArray _components;
+} Entity;
+
+Entity entity_create(size_t max_components_n) {
+    Entity result = {._components = array_new(sizeof(void *))};
+    array_increase_capacity(&result._components, max_components_n);
+    array_increase_length(&result._components, max_components_n);
+    for (size_t i = 0; i < max_components_n; i++) {
+        ARRAY_AT(void *, result._components, i) = NULL;
+    }
+    return result;
+}
+
+void entity_set_component(Entity e, size_t component_id, void *component) {
+    *((void **) array_at(e._components, component_id)) = component;
+}
+
+void *entity_get_component(Entity e, size_t component_id) {
+    return ARRAY_AT(void *, e._components, component_id);
+}
+
+typedef struct {
+    String text;
+} Name;
+
+typedef struct {
+    int x;
+} Position;
+
+void proto_system_update(Entity *e) {
+    printf(
+        "%s stands at X = %i\n",
+        string_to_c(((Name *)entity_get_component(*e, COMPONENT_NAME))->text),
+        ((Position *)entity_get_component(*e, COMPONENT_POSITION))->x
+    );
+}
+
 
 int main() {
     DynamicArray array = array_new(sizeof(int));
@@ -28,6 +70,19 @@ int main() {
 
     string_free(&s);
     printf("New string is '%s'\n", string_to_c(s));
+
+    Entity e = entity_create(COMPONENT_N);
+
+    Name *name = malloc(sizeof(Name));
+    name->text = string_from("girvel");
+    entity_set_component(e, COMPONENT_NAME, name);
+
+    Position *position = malloc(sizeof(Position));
+    position->x = 22;
+    entity_set_component(e, COMPONENT_POSITION, position);
+    printf("x is %i\n", ((Position *)entity_get_component(e, COMPONENT_POSITION))->x);
+
+    proto_system_update(&e);
 
     return 0;
 }
