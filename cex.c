c18febc9be7a4a38bec79d766f2f369e12451636
girvel <widauka@ya.ru>
2024-09-15 19:55:29 +0500

    ARRAY_AT macro

--- cex.c
+++ cex.c
@@ -26,10 +26,17 @@ void *array_at(DynamicArray array, size_t index) {
     return array._start + array._type_size * index;
 }
 
+#define ARRAY_AT(TYPE, ARRAY, INDEX) *((TYPE *)array_at((ARRAY), (INDEX)))
+
 int main() {
     DynamicArray array = array_create(sizeof(int), 10);
-    array._length = 1;
-    *((int *)array_at(array, 0)) = 32;
-    printf("array[0] = %i\n", *((int *)array_at(array, 0)));
+    array._length = 2;
+    ARRAY_AT(int, array, 0) = 32;
+    ARRAY_AT(int, array, 1) = 64;
+
+    for (size_t i = 0; i < array._length; i++) {
+        printf("array[%lu] = %i\n", i, ARRAY_AT(int, array, i));
+    }
+
     return 0;
 }
